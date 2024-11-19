1042d24d76bfe4a9aaf32bd7ed3454862a1c9c8a
DevTran <tan.tran@floatingcube.com>
2024-11-19 11:40:56 +0700

    final version before deploy staging

--- src/wp-content/themes/hello-elementor-child/includes/zippy-woo.php
+++ src/wp-content/themes/hello-elementor-child/includes/zippy-woo.php
@@ -2,13 +2,3 @@
 
 //fix mini cart
 function enqueue_wc_cart_fragments() { wp_enqueue_script( 'wc-cart-fragments' ); } add_action( 'wp_enqueue_scripts', 'enqueue_wc_cart_fragments' );
-
-
-// function remove_codeixer_menu() {
-//     // Remove the "Codeixer" menu
-//     remove_menu_page('codeixer');
-
-//     // Remove the "Dashboard" submenu under "Codeixer"
-//     remove_submenu_page('codeixer', 'codeixer-dashboard');
-// }
-// add_action('admin_menu', 'remove_codeixer_menu', 999);
